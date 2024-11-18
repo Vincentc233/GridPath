b0dcc175d3d46b2c4abd417528022ba22e130b1b
c4thyc <cathyc22@nycstudents.net>
2024-11-18 16:58:22 +0000

    aa

---
+++ GridPath.java
@@ -0,0 +1,16 @@
+public class GridPath
+{
+    public GridPath(int[][] values)
+    {
+        private int[][] grid;
+    }
+    grid = values;
+        public Location getNextLoc(int row, int col)
+        {
+            return null; 
+        }
+        public int sumPath(int row, int col) 
+        {
+            return 0;
+        }
+}
